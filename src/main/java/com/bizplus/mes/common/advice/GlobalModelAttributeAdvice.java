a0f3b6abd0e8a5d5bc9a6fbc4075973662a587e1
jh <juhyean8723@gmail.com>
2026-07-14 10:53:01 +0900

    - 알림 조각 추가. - 메인 레이아웃 스크립트 통합. - 메뉴 어드바이저 추가. - 체크박스 스크립트 추가. - 사이드바 리펙토링.

--- src/main/java/com/bizplus/mes/common/advice/GlobalModelAttributeAdvice.java
+++ src/main/java/com/bizplus/mes/common/advice/GlobalModelAttributeAdvice.java
@@ -1,15 +1,22 @@
 package com.bizplus.mes.common.advice;
 
+import com.bizplus.mes.domain.menu.Menu;
+import com.bizplus.mes.domain.menu.MenuRepository;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
+@RequiredArgsConstructor
 public class GlobalModelAttributeAdvice {
+
+    private final MenuRepository menuRepository;
 
     @ModelAttribute
     public void addPaginationPath(Model model, HttpServletRequest request) {
@@ -27,5 +34,13 @@ public class GlobalModelAttributeAdvice {
                 : requestPath + "?" + queryString;
 
         model.addAttribute("path", path);
+    }
+
+    @ModelAttribute
+    public void addMenu(Model model) {
+
+        List<Menu> menus = menuRepository.findAll();
+
+        model.addAttribute("menus", menus);
     }
 }
---
+++ src/main/resources/static/js/common/checkbox.js
@@ -0,0 +1,52 @@
+/**
+ * <div class="checkbox-group">
+ *     <table>
+ *         ...
+ *         <th>
+ *             <input type="checkbox" class="check-all">
+ *         </th>
+ *         ...
+ *         <td>
+ *             <input type="checkbox" class="check-item">
+ *         </td>
+ *     </table>
+ * </div>
+ */
+window.Checkbox = {
+
+    init() {
+
+        document.querySelectorAll('.checkbox-group').forEach(group => {
+
+            const checkAll = group.querySelector('.check-all');
+            const checkboxes = group.querySelectorAll('.check-item');
+
+            checkAll.addEventListener('change', function () {
+
+                checkboxes.forEach(checkbox => {
+                    checkbox.checked = this.checked;
+                });
+
+                checkAll.indeterminate = false;
+            });
+
+            checkboxes.forEach(checkbox => {
+
+                checkbox.addEventListener('change', () => {
+
+                    const checkedCount = group.querySelectorAll('.check-item:checked').length;
+                    const totalCount = checkboxes.length;
+
+                    checkAll.checked = checkedCount === totalCount;
+                    checkAll.indeterminate = checkedCount > 0 && checkedCount < totalCount;
+                });
+            });
+        });
+    },
+
+    getCheckedValues(group) {
+
+        return Array.from(group.querySelectorAll('.check-item:checked'))
+            .map(checkbox => checkbox.value);
+    }
+};

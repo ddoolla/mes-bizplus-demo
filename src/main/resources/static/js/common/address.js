a84e244087c378d7d928a87195f22efaa23eeea2
jh <juhyean8723@gmail.com>
2026-07-23 14:58:36 +0900

    - 주소검색 유틸 함수 분리.

---
+++ src/main/resources/static/js/common/address.js
@@ -0,0 +1,25 @@
+window.Mes = window.Mes || {};
+
+/**
+ * 개별 페이지에서 다음 우편번호 API 스크립트 추가
+ * <script src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script>
+ */
+Mes.Address = {
+
+    search(formId) {
+        new daum.Postcode({
+            oncomplete: function (data) {
+
+                const address = data.userSelectedType === 'R'
+                    ? data.roadAddress
+                    : data.jibunAddress;
+
+                const $form = $('#' + formId);
+
+                $form.find('[name="zipCode"]').val(data.zonecode);
+                $form.find('[name="address"]').val(address);
+                $form.find('[name="addressDetail"]').focus();
+            }
+        }).open();
+    }
+}

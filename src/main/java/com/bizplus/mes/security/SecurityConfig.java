97b85c6f6787ff4bb59c3ad548b2cf9da2814d01
jh <juhyean8723@gmail.com>
2026-09-14 13:02:47 +0900

    - 시큐리티 메서드 시큐리티 설정 추가.

--- src/main/java/com/bizplus/mes/security/SecurityConfig.java
+++ src/main/java/com/bizplus/mes/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,6 +16,7 @@ import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
